30796ca132193ddcbe6d0d58544ce0bc79021c05
Benny Tsai <104442934+bennytsai1234@users.noreply.github.com>
2026-08-11 16:42:05 +0800

    fix(ui): align source search editor density with bookshelf

--- lib/features/source_manager/views/source_edit_search.dart
+++ lib/features/source_manager/views/source_edit_search.dart
@@ -10,7 +10,7 @@ class SourceEditSearch extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(
-      padding: const EdgeInsets.all(AppSpacing.xl),
+      padding: const EdgeInsets.all(AppSpacing.lg),
       children: [
         RuleTextField(
           controller: controllers['searchUrl']!,
