feff82902635717e70bebf0e8fc8f591d5016c3e
Benny Tsai <104442934+bennytsai1234@users.noreply.github.com>
2026-08-11 16:42:15 +0800

    fix(ui): align source explore editor density with bookshelf

--- lib/features/source_manager/views/source_edit_explore.dart
+++ lib/features/source_manager/views/source_edit_explore.dart
@@ -10,7 +10,7 @@ class SourceEditExplore extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(
-      padding: const EdgeInsets.all(AppSpacing.xl),
+      padding: const EdgeInsets.all(AppSpacing.lg),
       children: [
         RuleTextField(
           controller: controllers['exploreUrl']!,
