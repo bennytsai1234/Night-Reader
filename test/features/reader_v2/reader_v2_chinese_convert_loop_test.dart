46d1671666c1346e9776bae335d83e1bdec81dba
蔡秉翰 <104442934+bennytsai1234@users.noreply.github.com>
2026-09-19 23:22:10 +0800

    test(reader): remove stale phase diagnostics

--- test/features/reader_v2/reader_v2_chinese_convert_loop_test.dart
+++ test/features/reader_v2/reader_v2_chinese_convert_loop_test.dart
@@ -15,7 +15,6 @@ import 'package:night_reader/features/reader_v2/layout/reader_v2_layout_spec.dar
 import 'package:night_reader/features/reader_v2/session/reader_v2_location.dart';
 import 'package:night_reader/features/reader_v2/session/reader_v2_progress_controller.dart';
 import 'package:night_reader/features/reader_v2/session/reader_v2_runtime.dart';
-import 'package:night_reader/features/reader_v2/session/reader_v2_state.dart';
 
 import 'reader_v2_state_transition_test_support.dart';
 
